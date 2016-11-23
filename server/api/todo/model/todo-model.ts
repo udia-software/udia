a6e733b7cb81f5ba95e03a2adc4eeb2c91561ea2
Alexander Wong <alexander@Alexanders-MacBook-Pro.local>
2016-11-22 22:27:08 -0700

    0

--- server/api/todo/model/todo-model.ts
+++
@@ -1,8 +0,0 @@
-import * as mongoose from 'mongoose';
-
-var schema = new mongoose.Schema({
-    todoMessage: {type: String, required: true, trim: true},
-    createdAt: {type: Date, default: Date.now}
-});
-
-export default schema;
