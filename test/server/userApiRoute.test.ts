962c2845bddf09dbe5fa25852b28039c86066b0f
Alexander Wong <admin@alexander-wong.com>
2016-12-31 00:19:24 -0700

    Added thing data integrity test, instead of only API verification

---
+++ test/server/userApiRoute.test.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by udia on 2016-12-30.
+ */
+import * as chai from "chai";
+import chaiHttp = require("chai-http");
+import app from "./app";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe("User API:", () => {
+  let user = {};
+
+});
