b6f02c200a916c4761269392dd681e32f113d071
Alexander Wong <admin@alexander-wong.com>
2017-02-20 11:33:28 -0700

    Refactored application to an umbrella application

---
+++ apps/udia/priv/repo/migrations/README.md
@@ -0,0 +1,6 @@
+## Migrations
+
+All database migrations go here.
+README file is placed here to track empty migrations folder in Git.
+
+* Migrations folder is required for `export MIX_ENV=test; mix test` to pass.
--- config/config.exs
+++ config/config.exs
@@ -1,27 +1,17 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
-config :udia,
-  ecto_repos: [Udia.Repo]
+# By default, the umbrella project as well as each child
+# application will require this configuration file, ensuring
+# they all use the same configuration. While one could
+# configure all applications here, we prefer to delegate
+# back to each application for organization purposes.
+import_config "../apps/*/config/config.exs"
 
-# Configures the endpoint
-config :udia, Udia.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "1qLidXrdNu8q/Zhvr0+HzpGFw0UK3lzbzIb70z0HENlHg5/i+k8CQMuDUodGnUb+",
-  render_errors: [view: Udia.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Udia.PubSub,
-           adapter: Phoenix.PubSub.PG2]
-
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+# Sample configuration (overrides the imported configuration above):
+#
+#     config :logger, :console,
+#       level: :info,
+#       format: "$date $time [$level] $metadata$message\n",
+#       metadata: [:user_id]
