4906001ca36f273ac818590e84175f03c43cf265
Alexander Wong <admin@alexander-wong.com>
2017-02-20 11:08:19 -0700

    Heroku deployment ready changes

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :udia, Udia.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("DOMAIN_NAME") || "udia.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :udia, Udia.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -55,7 +64,3 @@ config :logger, level: :info
 #
 #     config :udia, Udia.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
