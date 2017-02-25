e076b4510b121f6f7b2face2ab43814cf98987a3
Alexander Wong <admin@alexander-wong.com>
2017-02-25 00:40:26 -0700

    Activated socket code, added jQuery

--- apps/udia/web/channels/user_socket.ex
+++ apps/udia/web/channels/user_socket.ex
@@ -24,36 +24,26 @@ defmodule Udia.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Udia.RoomChannel
+  channel "room:*", Udia.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  @max_age 2 * 7 * 24 * 60 * 60
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_sage: @max_age) do
+      {:ok, user_id} ->
+        # If user id exists, assign to user id. This will be flushed out on login
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        # Allow anonymous socket join
+        {:ok, assign(socket, :user_id, nil)}
+    end
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     Udia.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def connect(_params, socket) do
+  end
+
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
--- apps/udia/web/static/js/app.js
+++ apps/udia/web/static/js/app.js
@@ -41,4 +41,4 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import socket from "./socket"
