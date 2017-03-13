9ed58934b3195449aea830ee6547234278357a22
Alexander Wong <admin@alexander-wong.com>
2017-03-13 14:04:50 -0600

    manifest.json -> cache_manifest.json

--- config/prod.exs
+++ config/prod.exs
@@ -37,7 +37,7 @@ config :udia, Udia.Web.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("DOMAIN_NAME") || "udia.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
