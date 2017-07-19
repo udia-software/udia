6c3017ce27ba9593422ae11e74c926137bff0cf1
Alexander Wong <admin@alexander-wong.com>
2017-07-19 12:41:23 -0600

    commented out unused preload methods

--- lib/udia/logs/comment.ex
+++ lib/udia/logs/comment.ex
@@ -57,37 +57,31 @@ defmodule Udia.Logs.Comment do
     timestamps()
   end
 
-  @doc """
-    Recursively load parents into the given struct until it hits nil
-  """
-  def load_parents(parent) do
-    load_parents(parent, 10)
-  end
+  # Commented these unused load methods, potentially for future use
+  # def load_parents(parent) do
+  #   load_parents(parent, 10)
+  # end
 
-  def load_parents(_, limit) when limit < 0, do: raise "Recursion limit reached"
+  # def load_parents(_, limit) when limit < 0, do: raise "Recursion limit reached"
 
-  def load_parents(%Comment{parent: %Ecto.Association.NotLoaded{}} = parent, limit) do
-    parent = parent |> Udia.Repo.preload(:parent)
-    Map.update!(parent, :parent, &Comment.load_parents(&1, limit - 1))
-  end
+  # def load_parents(%Comment{parent: %Ecto.Association.NotLoaded{}} = parent, limit) do
+  #   parent = parent |> Udia.Repo.preload(:parent)
+  #   Map.update!(parent, :parent, &Comment.load_parents(&1, limit - 1))
+  # end
 
-  def load_parents(nil, _), do: nil
+  # def load_parents(nil, _), do: nil
 
-  @doc """
-    Recursively loads children into the given struct until it hits []
-  """
-  def load_children(model), do: load_children(model, 10)
+  # def load_children(model), do: load_children(model, 10)
 
-  def load_children(_, limit) when limit < 0, do: raise "Recurison limit reached"
+  # def load_children(_, limit) when limit < 0, do: raise "Recurison limit reached"
 
-  def load_children(%Comment{children: %Ecto.Association.NotLoaded{}} = model, limit) do
-    model = model |> Udia.Repo.preload(:children)
-    Map.update!(model, :children, fn(list) ->
-      Enum.map(list, &Comment.load_children(&1, limit - 1))
-    end)
-  end
+  # def load_children(%Comment{children: %Ecto.Association.NotLoaded{}} = model, limit) do
+  #   model = model |> Udia.Repo.preload(:children)
+  #   Map.update!(model, :children, fn(list) ->
+  #     Enum.map(list, &Comment.load_children(&1, limit - 1))
+  #   end)
+  # end
 
-  @doc false
   def changeset(%Comment{} = comment, attrs) do
     comment
     |> cast(attrs, [:content, :type, :post_id, :parent_id])
