fadf3206f5e14552ed9a6bbced0f13b9659c9563
Alexander Wong <admin@alexander-wong.com>
2017-07-31 13:57:09 -0600

    Bumped to version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Udia.Mixfile do
   def project do
     [
       app: :udia,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
