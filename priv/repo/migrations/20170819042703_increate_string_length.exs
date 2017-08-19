6549c5c7b88228366f3c434dde0f7432dd2704b0
Alexander Wong <admin@alexander-wong.com>
2017-08-18 22:29:09 -0600

    change string length for all large datatypes

---
+++ priv/repo/migrations/20170819042703_increate_string_length.exs
@@ -0,0 +1,15 @@
+defmodule Udia.Repo.Migrations.IncreateStringLength do
+  use Ecto.Migration
+
+  def change do
+    alter table(:logs_comments) do
+      modify :content, :string, size: 20000
+    end
+    alter table(:logs_posts) do
+      modify :content, :string, size: 20000
+    end
+    alter table(:logs_journey) do
+      modify :description, :string, size: 20000
+    end
+  end
+end
